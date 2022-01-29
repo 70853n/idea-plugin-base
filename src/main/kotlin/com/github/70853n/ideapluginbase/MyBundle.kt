0e110f376b1f33ec3309b30f14535abdc0a44177
GitHub Action <action@github.com>
2022-01-29 15:51:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/70853n/ideapluginbase/MyBundle.kt
+++ src/main/kotlin/com/github/70853n/ideapluginbase/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.70853n.ideapluginbase
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/70853n/ideapluginbase/services/MyProjectService.kt
+++ src/main/kotlin/com/github/70853n/ideapluginbase/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.70853n.ideapluginbase.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.70853n.ideapluginbase.MyBundle
 
 class MyProjectService(project: Project) {
 
